5cf497a523b753bdd71db23a5e1bc7c3ee241d84
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-09-06 08:44:15 +0800

    feat: 完整版debounce

--- src/debounce.js
+++ src/debounce.js
@@ -1,10 +1,29 @@
 function debounce(fn, wait) {
   let timeout = null;
+  
   return function() {
-    let args = arguments;
+    const args = arguments;
     if (timeout) clearTimeout(timeout);
     timeout = setTimeout(() => {
       fn.apply(this, args);
+    }, wait);
+  }
+}
+
+// 加leading版本
+function debounce2(fn, wait, leading = true) {
+  let timer = null;
+  let led = leading;
+
+  return function def() {
+    const args = arguments;
+    timer && clearTimeout(timer);
+    led && fn.apply(this, args);
+
+    timer = setTimeout(() => {
+      timer = null;
+      !led && fn.apply(this, args);
+      led = false;
     }, wait);
   }
 }
