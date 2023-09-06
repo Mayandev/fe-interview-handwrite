0e74b253f485930d5fcca30267299e98915e2882
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-09-06 08:39:38 +0800

    feat: 箭头函数之下不需要特别地保存this

--- src/debounce.js
+++ src/debounce.js
@@ -1,11 +1,10 @@
 function debounce(fn, wait) {
   let timeout = null;
   return function() {
-    let context = this;
     let args = arguments;
     if (timeout) clearTimeout(timeout);
     timeout = setTimeout(() => {
-      fn.apply(context, args);
+      fn.apply(this, args);
     }, wait);
   }
 }
