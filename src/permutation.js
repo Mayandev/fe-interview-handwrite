8d36191d25a4ead292fe145bbd17428ef4a4db8d
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-09-11 19:46:20 +0800

    feat: 全排列

--- src/permutation.js
+++ src/permutation.js
@@ -16,3 +16,35 @@ const permutation = arr => {
 
     return res;
 };
+
+// 代码随想录的回溯算法
+const permutation2 = arr => {
+    const res = [];
+    const used = Array(arr.length).fill(false);
+
+    // 记录节点数组
+    const tmp = [];
+
+    // 回溯算法
+    const backtrackingFunc = ls => {
+        if (tmp.length === ls.length) {
+            // 必须copy一下才行
+            res.push([...tmp]);
+        } else {
+            ls.forEach((num, i) => {
+                // 回溯
+                if (!used[i]) {
+                    used[i] = true;
+                    tmp.push(num);
+                    backtrackingFunc(ls);
+                    used[i] = false;
+                    tmp.pop();
+                }
+            });
+        }
+    };
+
+    backtrackingFunc(arr);
+
+    return res;
+};
